e3e05525e53f2ccf41c7b8880ddee9a19cd09cad
LisaWijffels <r0657589@student.thomasmore.be>
2018-03-22 13:03:33 +0100

    Added view createpoll

--- pollapp/routes/index.js
+++ pollapp/routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/createpoll', function(req, res, next){
+  res.render('createpoll', {title: 'CreatePoll'});
+});
+
 module.exports = router;
